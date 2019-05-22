cb071ac8d62152c776b3dcd4bfbd9aba71c8c546
Benoit DAVID <benoit.davi@free.fr>
2019-05-22 19:09:13 +0000

    init

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+/*PhpDoc:
+name: index.php
+title: index.php - Tests de geovect
+*/
+?>
+<!DOCTYPE HTML><html><head><meta charset='UTF-8'><title>geovect</title></head><body>
+<h2>Tests</h2>
+<a href='gegeom'>gegeom</a><br>
+<a href='fcoll'>fcoll</a><br>
+<a href='geometry'>geometry</a><br>
+<a href='geom2d'>geom2d</a><br>
+<a href='coordsys'>coordsys</a><br>
+<a href='syscoord'>syscoord</a><br>
+<a href='ogr2php'>ogr2php</a><br>
