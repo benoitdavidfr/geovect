199105e79b400389973ec0b7d0450ade2ed8abec
Benoit DAVID <benoit.davi@free.fr>
2019-05-25 20:55:34 +0000

    add ugeojson.php

--- ugeojson.php
+++ ugeojson.php
@@ -8,20 +8,22 @@ doc: |
     - le schema, ex ne_110m
     - la collection/table, ex: coastline, admin_0_map_units
     - les critères de sélection, ex: bbox=[0,0,180,90], su_a3=FXX, su_a3(=[FXX,BEL]
+  Le protocole s'inspire aussi de WFS3.
   exemples:
     - http://localhost/geovect/ugeojson.php/ne_110m/collections/coastline/items?bbox=[0,0,180,90]
     - http://localhost/geovect/ugeojson.php/ne_110m/collections/admin_0_map_units/items?su_a3=FXX
   Points complémentaires:
-    - http://localhost/geovect/ugeojson.php/{schema}/collections/{table}/items fournit le contenu complet de la table
-    - http://localhost/geovect/ugeojson.php/{schema}/collections/{table}/schema fournit le schema de la table
-    - http://localhost/geovect/ugeojson.php/{schema}/collections/{table} fournit les MD de la table, au minimum une référence
-      vers son schema et une autre vers son contenu
+    - http://localhost/geovect/ugeojson.php/{schema}/collections/{collname}/items fournit le contenu complet de la table
+    - http://localhost/geovect/ugeojson.php/{schema}/collections/{collname}/schema fournit le schema de la table
+    - http://localhost/geovect/ugeojson.php/{schema}/collections/{collname} fournit les MD de la table, au minimum
+      une référence vers son schema et une autre vers son contenu
     - http://localhost/geovect/ugeojson.php/{schema}/collections fournit en JSON la liste des tables du schema
     - http://localhost/geovect/ugeojson.php/{schema} fournit en JSON la liste des tables du schema
     - http://localhost/geovect/ugeojson.php/api fournit la documentation de l'API
     - http://localhost/geovect/ugeojson.php fournit en JSON les MD du service et la liste des schemas
   Questions:
     - http://localhost/geovect/ugeojson.php/{schema} == http://localhost/geovect/ugeojson.php/{schema}/collections ?
+    - quelle MD ? où les stocker ?
 journal:
   25/5/2019:
     - première version
@@ -29,21 +31,29 @@ journal:
 require_once __DIR__.'/fcoll/database.inc.php';
 
 $dbParams = 'mysql://root@172.17.0.3/';
+MySql::open($dbParams);
 
 header('Content-type: application/json');
-//die(json_encode($_SERVER));
 
 $path = "http://$_SERVER[HTTP_HOST]$_SERVER[SCRIPT_NAME]";
 //die($path);
 
-if (!isset($_SERVER['PATH_INFO'])) { // racine = MD du serveur + exemples pour tests
+if (!isset($_SERVER['PATH_INFO'])) { // racine = MD du serveur + liste des schemas + exemples pour tests
+  $schemas = [];
+  $query = "select distinct table_schema from information_schema.columns where data_type='geometry'";
+  foreach(MySql::query($query) as $tuple) {
+    //echo "<pre>tuple="; print_r($tuple); echo "</pre>\n";
+    $schemas[$tuple['table_schema']] = [
+      'title'=> $tuple['table_schema'],
+      'href'=> "$path/$tuple[table_schema]",
+    ];
+  }
+  MySql::close();
   echo json_encode([
     'title'=> "Serveur UGeoJSON des FeatureCollection contenues dans la base MySql locale",
     'self'=> $path,
     'api'=> ['title'=> "documentation de l'API", 'href'=> "$path/api"],
-    'schemas'=> [
-      'ne_110m'=> ['title'=> "Base Natural Earth 1/110M", 'href'=> "$path/ne_110m"],
-    ],
+    'schemas'=> $schemas,
     'examples'=> [
       'coastline'=> [
         'title'=> "coastline",
@@ -67,22 +77,40 @@ if (!isset($_SERVER['PATH_INFO'])) { // racine = MD du serveur + exemples pour t
   ]);
   die();
 }
+function getIterator() {
+  $children = [];
+  Sql::open($this->params);
+  return new \ArrayIterator($children);
+}
 
-// "/api"
+// "/server" pour visualiser la variable $_SERVER
+if ($_SERVER['PATH_INFO'] == '/server') {
+  die(json_encode($_SERVER));
+}
+
+// "/api" - doc API à faire, utilité ?
 if ($_SERVER['PATH_INFO'] == '/api') {
   die(json_encode("API definition to be done"));
 }
 
-// "/{schema}" | "/{schema}/collections"
+// "/{schema}" | "/{schema}/collections" - liste des coillections d'un schema
 if (preg_match('!^/([^/]+)(/collections)?$!', $_SERVER['PATH_INFO'], $matches)) {
   $schemaname = $matches[1];
+  $collections = [];
+  $query = "select distinct table_name from information_schema.columns "
+    ."where table_schema='$schemaname' and data_type='geometry'";
+  foreach(MySql::query($query) as $tuple) {
+    //echo "<pre>tuple="; print_r($tuple); echo "</pre>\n";
+    $collections[$tuple['table_name']] = [
+      'title'=> "$tuple[table_name]",
+      'href'=> "$path/$schemaname/collections/$tuple[table_name]",
+    ];
+  }
+  MySql::close();
   die(json_encode([
     'title'=> $schemaname,
     'self'=> "$path/$schemaname",
-    'collections'=> [
-      'coastline'=> ['title'=> "coastline", 'href'=> "$path/$schemaname/collections/coastline"],
-      'admin_0_map_units'=> ['title'=> "admin_0_map_units", 'href'=> "$path/$schemaname/collections/admin_0_map_units"],
-    ],
+    'collections'=> $collections,
   ]));
 }
 
@@ -99,11 +127,40 @@ if (preg_match('!^/([^/]+)/collections/([^/]+)$!', $_SERVER['PATH_INFO'], $match
   die();
 }
 
+// conversion de type MySql -> JSON Schema
+function mysql2jsonDataType(string $mysqlDatatype) {
+  switch ($mysqlDatatype) {
+    case 'varchar': return 'string';
+    case 'decimal': return 'number';
+    case 'bigint': return 'integer';
+    case 'geometry': return ['$ref'=> 'http://json-schema.org/geojson/geometry.json#'];
+    default : return $mysqlDatatype;
+  }
+}
+
 // "/{schema}/collections/{collname}/schema"
 if (preg_match('!^/([^/]+)/collections/([^/]+)/schema$!', $_SERVER['PATH_INFO'], $matches)) {
   $schemaname = $matches[1];
   $collname = $matches[2];
-  die(json_encode("$schemaname.$collname's schema TO BE DONE"));
+  $query = "select ordinal_position, column_name, data_type from information_schema.columns "
+    ."where table_schema='$schemaname' and table_name='$collname' "
+    ."order by ordinal_position";
+  $properties = [];
+  foreach(MySql::query($query) as $tuple) {
+    //echo "<pre>tuple="; print_r($tuple); echo "</pre>\n";
+    $properties[$tuple['column_name']] = [
+      'type'=> mysql2jsonDataType($tuple['data_type']),
+    ];
+  }
+  MySql::close();
+  die(json_encode([
+    '$schema'=> 'http://json-schema.org/draft-04/schema#',
+    'id'=> "$path/$schemaname/collections/$collname/schema",
+    'title'=> "Schema de la collection $schemaname.$collname déduit du schema de la table dans MySql",
+    'type'=> 'object',
+    'required'=> array_keys($properties),
+    'properties'=> $properties,
+  ]));
 }
 
 if (preg_match('!^/([^/]+)/collections/([^/]+)/items$!', $_SERVER['PATH_INFO'], $matches)) { // "/{base}/{table}/items"
